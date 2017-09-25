a46f85c5974e4a5dc50dfb477ba37c5b28c30a44
Kya Suzuki <kyasuzuki2019@u.northwestern.edu>
2017-09-25 02:03:55 -0500

    first pop button changes color & increases the pop count by one when clicked - however current code is inefficient, should look into using an array of sorts

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -10,8 +10,30 @@ constructor() {
 
 }
 
+public ionicNamedColor: string = 'light';
+
 buttonClick(){
-console.log('crap');
+var count_string = document.getElementById('count').textContent;
+var count_number = Number(count_string);
+let clicked : boolean = false;
+
+if(this.ionicNamedColor === 'light') {
+       this.ionicNamedColor = 'secondary';
+       clicked = true;
+     } else {
+       this.ionicNamedColor = 'light';
+       clicked = false;
+     }
+
+if (clicked == true){
+document.getElementById('count').textContent = (count_number + 1).toString();
+}else{
+document.getElementById('count').textContent = (count_number - 1).toString();
 }
+
+console.log(count_number);
+console.log(count_string);
+}
+
 
 }
