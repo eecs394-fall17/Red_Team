1113b52e323edf20cb898f2f60bd310f2f68a265
Kya Suzuki <kyasuzuki2019@u.northwestern.edu>
2017-09-25 20:00:26 -0500

    population number updates correctly - need to fix the button color change functionality

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -17,7 +17,7 @@ var count_string = document.getElementById('count').textContent;
 var count_number = Number(count_string);
 let clicked : boolean = false;
 
-if(this.ionicNamedColor === 'light') {
+if(this.ionicNamedColor == 'light') {
        this.ionicNamedColor = 'secondary';
        clicked = true;
      } else {
@@ -30,10 +30,128 @@ document.getElementById('count').textContent = (count_number + 1).toString();
 }else{
 document.getElementById('count').textContent = (count_number - 1).toString();
 }
-
-console.log(count_number);
-console.log(count_string);
+console.log(this);
 }
 
+
+buttonClick1(){
+var count_string = document.getElementById('count1').textContent;
+var count_number = Number(count_string);
+let clicked : boolean = false;
+
+if(this.ionicNamedColor == 'light') {
+       this.ionicNamedColor = 'secondary';
+       clicked = true;
+     } else {
+       this.ionicNamedColor = 'light';
+       clicked = false;
+     }
+
+if (clicked == true){
+document.getElementById('count1').textContent = (count_number + 1).toString();
+}else{
+document.getElementById('count1').textContent = (count_number - 1).toString();
+}
+}
+
+buttonClick2(){
+var count_string = document.getElementById('count2').textContent;
+var count_number = Number(count_string);
+let clicked : boolean = false;
+
+if(this.ionicNamedColor == 'light') {
+       this.ionicNamedColor = 'secondary';
+       clicked = true;
+     } else {
+       this.ionicNamedColor = 'light';
+       clicked = false;
+     }
+
+if (clicked == true){
+document.getElementById('count2').textContent = (count_number + 1).toString();
+}else{
+document.getElementById('count2').textContent = (count_number - 1).toString();
+}
+}
+
+buttonClick3(){
+var count_string = document.getElementById('count3').textContent;
+var count_number = Number(count_string);
+let clicked : boolean = false;
+
+if(this.ionicNamedColor == 'light') {
+       this.ionicNamedColor = 'secondary';
+       clicked = true;
+     } else {
+       this.ionicNamedColor = 'light';
+       clicked = false;
+     }
+
+if (clicked == true){
+document.getElementById('count3').textContent = (count_number + 1).toString();
+}else{
+document.getElementById('count3').textContent = (count_number - 1).toString();
+}
+}
+
+buttonClick4(){
+var count_string = document.getElementById('count4').textContent;
+var count_number = Number(count_string);
+let clicked : boolean = false;
+
+if(this.ionicNamedColor == 'light') {
+       this.ionicNamedColor = 'secondary';
+       clicked = true;
+     } else {
+       this.ionicNamedColor = 'light';
+       clicked = false;
+     }
+
+if (clicked == true){
+document.getElementById('count4').textContent = (count_number + 1).toString();
+}else{
+document.getElementById('count4').textContent = (count_number - 1).toString();
+}
+}
+
+buttonClick5(){
+var count_string = document.getElementById('count5').textContent;
+var count_number = Number(count_string);
+let clicked : boolean = false;
+
+if(this.ionicNamedColor == 'light') {
+       this.ionicNamedColor = 'secondary';
+       clicked = true;
+     } else {
+       this.ionicNamedColor = 'light';
+       clicked = false;
+     }
+
+if (clicked == true){
+document.getElementById('count5').textContent = (count_number + 1).toString();
+}else{
+document.getElementById('count5').textContent = (count_number - 1).toString();
+}
+}
+
+buttonClick6(){
+var count_string = document.getElementById('count6').textContent;
+var count_number = Number(count_string);
+let clicked : boolean = false;
+
+if(this.ionicNamedColor == 'light') {
+       this.ionicNamedColor = 'secondary';
+       clicked = true;
+     } else {
+       this.ionicNamedColor = 'light';
+       clicked = false;
+     }
+
+if (clicked == true){
+document.getElementById('count6').textContent = (count_number + 1).toString();
+}else{
+document.getElementById('count6').textContent = (count_number - 1).toString();
+}
+}
 
 }
