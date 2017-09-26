c6068d91d01d255afc86fca105b402adf5626d73
Kya Suzuki <kyasuzuki2019@u.northwestern.edu>
2017-09-25 23:52:15 -0500

    dropdown buttons and pop? buttons fixed - button event actions now independent of one another

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -9,15 +9,58 @@ templateUrl: 'hello-ionic.html'
 export class HelloIonicPage {
 
 data: Array<{ details: string, icon: string, showDetails: boolean}> = [];
+data1: Array<{ details: string, icon: string, showDetails: boolean}> = [];
+data2: Array<{ details: string, icon: string, showDetails: boolean}> = [];
+data3: Array<{ details: string, icon: string, showDetails: boolean}> = [];
+data4: Array<{ details: string, icon: string, showDetails: boolean}> = [];
+data5: Array<{ details: string, icon: string, showDetails: boolean}> = [];
+data6: Array<{ details: string, icon: string, showDetails: boolean}> = [];
 
 
- constructor(public navCtrl: NavController) {
-     this.data.push({
-         details: 'details dropdown',
-         icon: 'ios-add-circle-outline',
-         showDetails: false
-       });
- }
+
+constructor(public navCtrl: NavController) {
+this.data.push({
+details: 'The first round of Club Tennis Tryouts will be held at SPAC and last 3 hours.',
+icon: 'ios-add-circle-outline',
+showDetails: false
+});
+
+this.data1.push({
+details: 'Mr.Smith will be hosting a creative writing workshop in Fisk 102. The theme is Halloween.',
+icon: 'ios-add-circle-outline',
+showDetails: false
+});
+
+this.data2.push({
+details: 'The Chinese Students Association is giving out free dumplings by the arch from 11am to 3pm in honor of the New Year. ',
+icon: 'ios-add-circle-outline',
+showDetails: false
+});
+
+this.data3.push({
+details: 'Representatives from Microsoft headquarters will be available to answer questions and accept resumes from 12pm to 2pm in Tech LR4',
+icon: 'ios-add-circle-outline',
+showDetails: false
+});
+
+this.data4.push({
+details: 'NU Tutors is looking to hire more staff. Come to Annenberg Room 21 to hear more!',
+icon: 'ios-add-circle-outline',
+showDetails: false
+});
+
+this.data5.push({
+details: 'The all male Acapella group will be performing their first show of 2017 tonight in the Norris Louis Room',
+icon: 'ios-add-circle-outline',
+showDetails: false
+});
+
+this.data6.push({
+details: 'Do you like improv? Do you like to laugh? Come checkout NSTV and meet individuals involved in improv groups on campus!',
+icon: 'ios-add-circle-outline',
+showDetails: false
+});
+}
 
  toggleDetails(data) {
    if (data.showDetails) {
@@ -30,6 +73,12 @@ data: Array<{ details: string, icon: string, showDetails: boolean}> = [];
  }
 
 public ionicNamedColor: string = 'light';
+public ionicNamedColor1: string = 'light';
+public ionicNamedColor2: string = 'light';
+public ionicNamedColor3: string = 'light';
+public ionicNamedColor4: string = 'light';
+public ionicNamedColor5: string = 'light';
+public ionicNamedColor6: string = 'light';
 
 buttonClick(){
 var count_string = document.getElementById('count').textContent;
@@ -58,11 +107,11 @@ var count_string = document.getElementById('count1').textContent;
 var count_number = Number(count_string);
 let clicked : boolean = false;
 
-if(this.ionicNamedColor == 'light') {
-       this.ionicNamedColor = 'secondary';
+if(this.ionicNamedColor1 == 'light') {
+       this.ionicNamedColor1 = 'secondary';
        clicked = true;
      } else {
-       this.ionicNamedColor = 'light';
+       this.ionicNamedColor1 = 'light';
        clicked = false;
      }
 
@@ -78,11 +127,11 @@ var count_string = document.getElementById('count2').textContent;
 var count_number = Number(count_string);
 let clicked : boolean = false;
 
-if(this.ionicNamedColor == 'light') {
-       this.ionicNamedColor = 'secondary';
+if(this.ionicNamedColor2 == 'light') {
+       this.ionicNamedColor2 = 'secondary';
        clicked = true;
      } else {
-       this.ionicNamedColor = 'light';
+       this.ionicNamedColor2 = 'light';
        clicked = false;
      }
 
@@ -98,11 +147,11 @@ var count_string = document.getElementById('count3').textContent;
 var count_number = Number(count_string);
 let clicked : boolean = false;
 
-if(this.ionicNamedColor == 'light') {
-       this.ionicNamedColor = 'secondary';
+if(this.ionicNamedColor3 == 'light') {
+       this.ionicNamedColor3 = 'secondary';
        clicked = true;
      } else {
-       this.ionicNamedColor = 'light';
+       this.ionicNamedColor3 = 'light';
        clicked = false;
      }
 
@@ -118,11 +167,11 @@ var count_string = document.getElementById('count4').textContent;
 var count_number = Number(count_string);
 let clicked : boolean = false;
 
-if(this.ionicNamedColor == 'light') {
-       this.ionicNamedColor = 'secondary';
+if(this.ionicNamedColor4 == 'light') {
+       this.ionicNamedColor4 = 'secondary';
        clicked = true;
      } else {
-       this.ionicNamedColor = 'light';
+       this.ionicNamedColor4 = 'light';
        clicked = false;
      }
 
@@ -138,11 +187,11 @@ var count_string = document.getElementById('count5').textContent;
 var count_number = Number(count_string);
 let clicked : boolean = false;
 
-if(this.ionicNamedColor == 'light') {
-       this.ionicNamedColor = 'secondary';
+if(this.ionicNamedColor5 == 'light') {
+       this.ionicNamedColor5 = 'secondary';
        clicked = true;
      } else {
-       this.ionicNamedColor = 'light';
+       this.ionicNamedColor5 = 'light';
        clicked = false;
      }
 
@@ -158,11 +207,11 @@ var count_string = document.getElementById('count6').textContent;
 var count_number = Number(count_string);
 let clicked : boolean = false;
 
-if(this.ionicNamedColor == 'light') {
-       this.ionicNamedColor = 'secondary';
+if(this.ionicNamedColor6 == 'light') {
+       this.ionicNamedColor6 = 'secondary';
        clicked = true;
      } else {
-       this.ionicNamedColor = 'light';
+       this.ionicNamedColor6 = 'light';
        clicked = false;
      }
 
